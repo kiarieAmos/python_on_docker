1d10232efa4f7c402b8d0f17f1530cfabf6731e0
ubuntu-kiarieAmos <amosmkiarie@gmail.com>
2025-10-20 23:47:40 +0300

    hello_world in python

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+my_string = 'Hello World'
+greetings = 'The name is Amos Kiarie, A data engineer specialising in Linux, Python and SQL'
+sign_of = 'Happy to be here'
+
+final_output = my_string + '\n' +greetings + '\n' + sign_of +'.'
+
+
+print(final_output)
